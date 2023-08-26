e7a6d2beb4e545d24a42777e80514acb6a47a7a5
Waydj <farhminait@gmail.com>
2023-08-26 13:39:19 +0200

    generate sqlite with psisma, add register, login

---
+++ controllers/users.js
@@ -0,0 +1,98 @@
+const { prisma } = require("../prisma/prisma-client");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+/**
+ * @route POST api/user/login
+ * @desc вход
+ * @access public
+ * */
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Пожалуйста заполните обязательные поля" });
+  }
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  const isPasswordCorrect =
+    user && (await bcrypt.compare(password, user.password));
+  const secret = process.env.JWT_SECRET;
+
+  if (user && isPasswordCorrect && secret) {
+    res.status(200).json({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      token: jwt.sign({ id: user.id }, secret, { expiresIn: 30 }),
+    });
+  } else {
+    return res.status(400).json({ message: "Неверный Логин или Пароль" });
+  }
+};
+
+/**
+ * @route POST api/user/register
+ * @desc регистрация
+ * @access public
+ * */
+const register = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  if (!email || !password || !name) {
+    return res
+      .status(400)
+      .json({ message: "Пожалуйста заполните обязательные поля" });
+  }
+
+  const registeredUser = await prisma.user.findFirst({
+    where: { email },
+  });
+
+  if (registeredUser) {
+    return res
+      .status(400)
+      .json({ message: "Пользователь с таким email уже существует" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  const secret = process.env.JWT_SECRET;
+
+  if (user && secret) {
+    res.status(201).json({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      token: jwt.sign({ id: user.id }, secret, { expiresIn: 30 }),
+    });
+  } else {
+    return res.status(400).json({ message: "Не удалось создать пользователя" });
+  }
+};
+
+const current = async (req, res) => {
+  res.send("current");
+};
+
+module.exports = {
+  login,
+  register,
+  current,
+};

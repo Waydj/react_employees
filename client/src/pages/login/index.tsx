002e73fa363986f05e1e1a650c5108e782d2ae20
Waydj <farhminait@gmail.com>
2023-09-05 00:06:20 +0200

    add login and register pages

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,10 +1,32 @@
+import { Card, Form, Row, Space, Typography } from "antd"
 import { Header } from "../../components/header"
 import { Layout } from "../../components/layout"
+import CustomInput from "../../components/custom-input"
+import PasswordInput from "../../components/password-input"
+import { CustomButton } from "../../components/custom-button"
+import { Link } from "react-router-dom"
+import { Paths } from "../../paths"
 
 export const Login = () => {
   return (
     <Layout>
       <Header />
+      <Row align="middle" justify="center">
+        <Card title="Войдите" style={{ width: "30rem" }}>
+          <Form onFinish={() => {}}>
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Пароль" />
+            <CustomButton type="primary" htmlType="submit">
+              Войти
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
     </Layout>
   )
 }
--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,3 +1,37 @@
+import { Card, Form, Row, Space, Typography } from "antd"
+import { Header } from "../../components/header"
+import { Layout } from "../../components/layout"
+import CustomInput from "../../components/custom-input"
+import PasswordInput from "../../components/password-input"
+import { CustomButton } from "../../components/custom-button"
+import { Link } from "react-router-dom"
+import { Paths } from "../../paths"
+
 export const Register = () => {
-  return <div>register</div>
+  return (
+    <Layout>
+      <Header />
+      <Row align="middle" justify="center">
+        <Card title="Регистрация" style={{ width: "30rem" }}>
+          <Form onFinish={() => {}}>
+            <CustomInput name="name" placeholder="Имя" />
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Пароль" />
+            <PasswordInput
+              name="confirmPassword"
+              placeholder="Повторите пароль"
+            />
+            <CustomButton type="primary" htmlType="submit">
+              Зарегистрироваться
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Уже Зарегистрированны? <Link to={Paths.login}>Войдите</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
+    </Layout>
+  )
 }

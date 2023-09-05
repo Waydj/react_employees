4a84b29eea4bcc6a05e96756cfc85065c6962c66
Waydj <farhminait@gmail.com>
2023-09-05 22:02:57 +0200

    add authSlice, error-message

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
@@ -8,6 +8,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,19 +1,41 @@
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import { Card, Form, Row, Space, Typography } from "antd"
 import { Header } from "../../components/header"
 import { Layout } from "../../components/layout"
 import CustomInput from "../../components/custom-input"
 import PasswordInput from "../../components/password-input"
 import { CustomButton } from "../../components/custom-button"
-import { Link } from "react-router-dom"
 import { Paths } from "../../paths"
+import { UserData, useLoginMutation } from "../../app/services/auth"
+import { isErrorWithMessage } from "../../utils/is-error-with-message"
+import { ErrorMessage } from "../../components/error-message"
 
 export const Login = () => {
+  const navigate = useNavigate()
+  const [loginUser, loginUserResult] = useLoginMutation()
+  const [error, setError] = useState("")
+
+  const login = async (data: UserData) => {
+    try {
+      await loginUser(data).unwrap()
+      navigate("/")
+    } catch (err) {
+      const isError = isErrorWithMessage(err)
+      if (isError) {
+        setError(err.data.message)
+      } else {
+        setError("Неизвестная ошибка")
+      }
+    }
+  }
+
   return (
     <Layout>
       <Header />
       <Row align="middle" justify="center">
         <Card title="Войдите" style={{ width: "30rem" }}>
-          <Form onFinish={() => {}}>
+          <Form onFinish={login}>
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Пароль" />
             <CustomButton type="primary" htmlType="submit">
@@ -24,6 +46,7 @@ export const Login = () => {
             <Typography.Text>
               Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
